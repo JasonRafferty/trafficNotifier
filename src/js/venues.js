7719e5ecb0f354f1a39659182a9b3186881a1dd7
Jason Rafferty <jasonraff@hotmail.co.uk>
2025-03-14 00:22:05 +0000

    updated venues

--- src/js/venues.js
+++ src/js/venues.js
@@ -15,46 +15,64 @@ export const venueCapacities = {
   KovZ9177z1y: { capacity: 41500, type: "football" }, // Stamford Bridge
   KovZ9177z7g: { capacity: 25000, type: "music" }, // The O2 Arena
   KovZ9177z8p: { capacity: 25000, type: "football" }, // The Valley (Charlton)
+  KovZ9177z4X: { capacity: 40000, type: "football" }, // Craven Cottage (Fulham)
+  KovZ9177z5X: { capacity: 30500, type: "football" }, // Selhurst Park (Crystal Palace)
 
   // Birmingham
   KovZ9177z9c: { capacity: 42785, type: "football" }, // Villa Park (Aston Villa)
   KovZ9177z9B: { capacity: 30016, type: "football" }, // St Andrew's (Birmingham City)
   KovZ9177z7n: { capacity: 30016, type: "cricket" }, // Edgbaston Cricket Ground
+  KovZ9177z7Y: { capacity: 30000, type: "football" }, // Alexander Stadium (Athletics & Rugby)
 
   // Liverpool
   KovZ9177z6t: { capacity: 53394, type: "football" }, // Anfield
   KovZ9177z6u: { capacity: 39000, type: "football" }, // Goodison Park
+  KovZ9177z6V: { capacity: 54074, type: "music" }, // Bramley-Moore Dock Stadium (Everton, future stadium)
 
   // Newcastle
   KovZ9177z8n: { capacity: 52305, type: "football" }, // St James' Park
+  KovZ9177z8M: { capacity: 36500, type: "rugby" }, // Kingston Park Stadium (Rugby)
 
   // Sunderland
   KovZ9177z8y: { capacity: 49000, type: "football" }, // Stadium of Light
 
   // Cardiff
   KovZ9177z9q: { capacity: 74500, type: "rugby" }, // Principality Stadium (Millennium Stadium)
+  KovZ9177z9A: { capacity: 33280, type: "football" }, // Cardiff City Stadium
 
   // Glasgow
   KovZ9177z9r: { capacity: 60411, type: "football" }, // Celtic Park
   KovZ9177z9s: { capacity: 51866, type: "football" }, // Hampden Park
   KovZ9177z9t: { capacity: 50817, type: "football" }, // Ibrox Stadium
+  KovZ9177z9G: { capacity: 25000, type: "music" }, // The SSE Hydro (Glasgow Music Arena)
 
   // Leeds
   KovZ9177z9x: { capacity: 37890, type: "football" }, // Elland Road
+  KovZ9177z9Y: { capacity: 23000, type: "cricket" }, // Headingley Cricket Ground
 
   // Sheffield
   KovZ9177z4d: { capacity: 32702, type: "football" }, // Bramall Lane
   KovZ9177z4s: { capacity: 39732, type: "football" }, // Hillsborough Stadium
+  KovZ9177z4P: { capacity: 28500, type: "cricket" }, // Sheffield Hallam Stadium
+
+  // Edinburgh
+  KovZ9177z9E: { capacity: 67800, type: "rugby" }, // Murrayfield Stadium (Rugby)
 
   // Nottingham
   KovZ9177z3b: { capacity: 30445, type: "football" }, // City Ground
+  KovZ9177z3R: { capacity: 21000, type: "cricket" }, // Trent Bridge Cricket Ground
 
   // Leicester
   KovZ9177z2f: { capacity: 32273, type: "football" }, // King Power Stadium
+  KovZ9177z2T: { capacity: 25000, type: "rugby" }, // Welford Road Stadium (Leicester Tigers)
 
   // Southampton
   KovZ9177z2h: { capacity: 32384, type: "football" }, // St Mary's Stadium
+  KovZ9177z2S: { capacity: 25000, type: "cricket" }, // The Ageas Bowl (Cricket)
 
   // Wolverhampton
   KovZ9177z2j: { capacity: 32050, type: "football" }, // Molineux Stadium
+
+  // Belfast (Northern Ireland)
+  KovZ9177zNI: { capacity: 32000, type: "rugby" }, // Kingspan Stadium (Ulster Rugby)
 };
